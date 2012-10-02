e46efa59bf3a4c3f90500f19368e22ef649376e3
jtf <jtf@zim>
2012-10-02 18:15:42 +0200

    first version

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(calculator-use-menu nil)
+ '(ecb-layout-window-sizes (quote (("left8" (0.2986111111111111 . 0.23333333333333334) (0.2986111111111111 . 0.25) (0.2986111111111111 . 0.2833333333333333) (0.2986111111111111 . 0.21666666666666667)))))
+ '(ecb-options-version "2.40")
+ '(fringe-mode (quote (5 . 5)) nil (fringe))
+ '(indicate-buffer-boundaries (quote left))
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(overflow-newline-into-fringe t)
+ '(scroll-bar-mode (quote right))
+ '(tool-bar-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(fringe ((((class color) (background light)) (:background "#dcedff")))))
