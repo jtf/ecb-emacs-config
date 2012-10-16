6c997e9672d13a0b72cf4eef8346c269fe745f57
jtf <jtf@zim>
2012-10-16 20:02:53 +0200

    more customize

--- custom.el
+++ custom.el
@@ -12,10 +12,12 @@
  '(inhibit-startup-screen t)
  '(overflow-newline-into-fringe t)
  '(scroll-bar-mode (quote right))
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(whitespace-style (quote (face tabs spaces trailing lines space-before-tab newline indentation empty space-after-tab space-mark tab-mark))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(fringe ((((class color) (background light)) (:background "#dcedff")))))
+ '(fringe ((((class color) (background light)) (:background "#dcedff"))))
+ '(whitespace-tab ((((class color) (background light)) (:foreground "lightgray")))))
